dcc36f7f3d338c55e1819f2eca745f290176da4d
jonaskankode <j.storesund16@gmail.com>
2022-07-27 14:59:21 +0200

    endringer

---
+++ prosjekt-fotball_nettsted/html/svelte-app-nye-drakter/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	let drakter = [ "https://store.liverpoolfc.com/media/catalog/product/cache/042594ffe2aeaa6bf11576291063d119/d/j/dj7647r_1_copy.jpg", "https://pbs.twimg.com/media/E6UpSQ3WQAAyrV6.jpg", "https://i2-prod.football.london/incoming/article23896915.ece/ALTERNATES/s1200c/1_Arsenal-22-23-Home-Kit-3.jpg"]
+	let i = 0
+	const visBilde = () => {
+		drakter[i] = drakter[i++] 
+		} 
+	if(i = drakter.length - 1){
+			i=0}
+	
+	//Fikk litt drlig tid, dermed klarte ikke å resette i haha
+		
+
+</script>
+
+<button on:click={visBilde}> Vis neste drakt </button>
+
+
+<br>
+
+<div>
+	<img src={drakter[i]} alt="drakter" style="width:50%; height:50%;">
+</div>
+
+
+
+
---
+++ prosjekt-fotball_nettsted/html/svelte-app-spillere/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	let spillere = [{navn:"Mohamed Salah", fornavn:"Mohamed", etternavn:"Salah", alder:"30", nasjonalitet:"Egypt", høyde:"175 cm", vekt:"71 kg", assister: "13" ,mål: "23", kamper: "35", bilde:"https://media.api-sports.io/football/players/306.png"}, {navn:"Cristiano Ronaldo", fornavn:"Cristiano", etternavn:"Ronaldo", alder:"37", nasjonalitet:"Portugal", høyde: "187 cm", vekt: "85 kg", mål: "18", kamper: "31", assister: "3", bilde: "https://media.api-sports.io/football/players/874.png"}]
+	
+	let i = 0
+	const finnSpiller = () =>{
+		i++
+		spillere.navn = spillere.navn[i]
+	}
+
+</script>
+
+<tag>Søk på en fotballspiller: </tag><input bind:value={spillere.navn}> <br>
+
+
+
+
+{#if spillere.navn === spillere[0].navn}
+<h2>{spillere[0].navn}</h2>
+<img src={spillere[0].bilde} alt="spillerbilde"> <br>
+		Alder: {spillere[0].alder} <br>
+		Nasjonalitet: {spillere[0].nasjonalitet} <br>
+		Høyde: {spillere[0].høyde} <br>
+		Kamper: {spillere[0].kamper} <br>
+		Mål: {spillere[0].mål} <br>
+		Assister: {spillere[0].assister}
+
+{:else if spillere.navn === spillere[1].navn}
+		<img src={spillere[1].bilde} alt="spillerbilde"> <br>
+		Alder: {spillere[1].alder} <br>
+		Nasjonalitet: {spillere[1].nasjonalitet} <br>
+		Høyde: {spillere[1].høyde} <br>
+		Kamper: {spillere[1].kamper} <br>
+		Mål: {spillere[1].mål} <br>
+		Assister: {spillere[1].assister}
+{:else}
+	
+{/if}
+
+
+
+
+
+
